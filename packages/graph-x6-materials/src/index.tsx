ea739ff5459d7fba2e964c0433ff392e0dc78b55
Luke <jun.lukesmail@gmail.com>
2023-03-20 15:01:53 +0800

    graph x6 materials (#47)
    * feat: graph x6 materials

---
+++ packages/graph-x6-materials/src/index.tsx
@@ -0,0 +1,9 @@
+export { default as Line } from './components/line';
+export { default as SendEmail } from './components/send-email';
+export { default as StartEnd } from './components/start-end';
+
+const bizCssPrefix = 'bizpack';
+
+export {
+  bizCssPrefix
+}
