2fb029245524085e25ad6fc0b5664f5c9d88b8c5
果嘉 <591687284@qq.com>
2023-03-22 10:26:47 +0800

    fix: 代码style规范, 删除.umi编译变更 添加ignore

--- packages/fusion-ui/docs/src/.umi/dumi/layout.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import React from 'react';
-import config from '@@/dumi/config';
-import demos from '@@/dumi/demos';
-import apis from '@@/dumi/apis';
-import Layout from '/Users/guojia/Desktop/WorkApp/低代码开源域/lowcode-materials/packages/fusion-ui/node_modules/@umijs/preset-dumi/lib/theme/layout';
-
-export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
