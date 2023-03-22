eb6b3ac6836fa6e0858c59f6a3a76c5237eda5cb
果嘉 <591687284@qq.com>
2023-03-22 10:26:47 +0800

    fix: 代码style规范, 删除.umi编译变更 添加ignore

--- packages/fusion-ui/docs/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/guojia/Desktop/WorkApp/低代码开源域/lowcode-materials/packages/fusion-ui/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
