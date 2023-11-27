503dca7c3e51718beafedfd2c50525f215fb69d9
Charmtiger <charmtiger@gmail.com>
2023-11-27 14:40:57 +0800

    feat: 更新antd4->5

--- packages/antd-lowcode-materials/build.lowcode.js
+++ packages/antd-lowcode-materials/build.lowcode.js
@@ -54,18 +54,19 @@ module.exports = {
                 package: 'iconfont-icons',
                 urls: '//at.alicdn.com/t/font_2369445_ukrtsovd92r.js',
               },
-              {
-                package: '@ant-design/icons',
-                version: '4.7.0',
-                urls: [`//g.alicdn.com/code/npm/@ali/ant-design-icons-cdn/4.5.0/index.umd.min.js`],
-                library: 'icons',
-              },
+              // {
+              //   package: '@ant-design/icons',
+              //   version: '4.7.0',
+              //   urls: [`//g.alicdn.com/code/npm/@ali/ant-design-icons-cdn/4.5.0/index.umd.min.js`],
+              //   library: 'icons',
+              // },
               {
                 package: 'antd',
-                version: '4.23.0',
+                version: '5.9.0',
                 urls: [
-                  `//g.alicdn.com/code/lib/antd/4.23.0/antd.min.js`,
-                  `//g.alicdn.com/code/lib/antd/4.23.0/antd.min.css`,
+                  '//cdn.bootcdn.net/ajax/libs/dayjs/1.11.9/dayjs.min.js',
+                  `//cdn.bootcdn.net/ajax/libs/antd/5.9.0/antd.js`,
+                  `//cdn.bootcdn.net/ajax/libs/antd/5.9.0/reset.css`,
                 ],
                 library: 'antd',
               },
