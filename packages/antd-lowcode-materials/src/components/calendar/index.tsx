cf87782dcc089dc51ece697eb8f5d4b06b477d95
Zhizhi_2986 <yliu2986@gmail.com>
2024-03-22 10:30:52 +0800

    Revert "fix: 日历组件面板不能切换"
    This reverts commit f9bac695821dee48abe6350d222852ebeb53b83c.

--- packages/antd-lowcode-materials/src/components/calendar/index.tsx
+++ packages/antd-lowcode-materials/src/components/calendar/index.tsx
@@ -1,20 +1,10 @@
 import { Calendar as OriginalCalendar } from 'antd';
 import { withMomentProps } from '../../utils/hoc';
 
-const InitCalendar = withMomentProps(OriginalCalendar, [
+const Calendar = withMomentProps(OriginalCalendar, [
   'defaultValue',
   'validRange',
   'value.defaultValue',
 ]);
-
-const Calendar = (props: any) => {
-  return (
-    <InitCalendar
-      {...props}
-      mode={props.showMode ? props.mode : undefined}
-      onPanelChange={props?.onPanelChange}
-    ></InitCalendar>
-  );
-};
 
 export default Calendar;
